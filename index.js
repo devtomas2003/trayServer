5ef5b07c11e7277b61b39c7753bd93e0d42a589b
devtomas2003 <54170951+devtomas2003@users.noreply.github.com>
2020-11-30 15:53:31 +0000

    Inicio do servidor Web

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const { resolve } = require('path');
 const { app, Menu, Tray } = require('electron');
-
+const { spawn } = require('child_process');
+const static = require('node-static');
+const file = new static.Server(`./public`)
+if (app.dock) {
+    app.dock.hide();
+}
 app.on('ready', () => {
+    require('http').createServer(function (request, response) {
+        request.addListener('end', function () {
+            file.serve(request, response);
+        }).resume();
+    }).listen(8080);
     const tray = new Tray(resolve(__dirname, 'assets', 'phone.png'));
 
     const contextMenu = Menu.buildFromTemplate([
-        { label: 'dasd', click: () => {
-            console.log("dfd");
-        }}
+        { label: 'Abrir Programador', click: () => {
+            spawn('yarn', [path], { shell: true });
+        }},
+        { label: 'Parar Programador', click: () => {
+            console.log("ff");
+        }},
     ]);
     tray.setContextMenu(contextMenu);
 });
