73b93cac04e94bac154449178557e8aa15b36cc0
devtomas2003 <54170951+devtomas2003@users.noreply.github.com>
2020-11-30 16:19:26 +0000

    Continuação do desenvolvimento do trayMenu

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 const { resolve } = require('path');
-const { app, Menu, Tray } = require('electron');
-const { spawn } = require('child_process');
+const { app, Menu, Tray, shell } = require('electron');
 const static = require('node-static');
 const file = new static.Server(`./public`)
 if (app.dock) {
     app.dock.hide();
 }
+const staticServer = require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
 app.on('ready', () => {
-    require('http').createServer(function (request, response) {
-        request.addListener('end', function () {
-            file.serve(request, response);
-        }).resume();
-    }).listen(8080);
+    shell.openExternal('http://127.0.0.1:8080/');
     const tray = new Tray(resolve(__dirname, 'assets', 'phone.png'));
-
+    renderTray(tray, true);
+});
+function renderTray(tray = mainTray, serviceStatus){
+    var status = '';
+    if(serviceStatus){
+        status = 'Parar Serviço';
+    }else{
+        status = 'Iniciar Serviço';
+    }
     const contextMenu = Menu.buildFromTemplate([
         { label: 'Abrir Programador', click: () => {
-            spawn('yarn', [path], { shell: true });
+            shell.openExternal('http://127.0.0.1:8080/');
         }},
-        { label: 'Parar Programador', click: () => {
-            console.log("ff");
+        { label: status, click: () => {
+            if(serviceStatus){
+                staticServer.close();
+                renderTray(tray, false);
+            }else{
+                staticServer.resume();
+                renderTray(tray, true);
+            }
+
         }},
+        { label: 'Terminar Programador', click: () => {
+            app.exit();
+        }}
     ]);
     tray.setContextMenu(contextMenu);
-});
-
+}
